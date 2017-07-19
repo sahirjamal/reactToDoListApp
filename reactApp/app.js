4a7bab88fe7210443e354cd40a06418397fdc53b
Sahir Jamal <sahir.jamal4@gmail.com>
2017-07-19 16:54:02 -0700

    basic structure and first component

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const dummyData = ["sahir", "sahir1", "shridhar", "hello", "fuck shridhar", "lisa wont survive in GoT", 
+				"bilbo baggins", "THE HOUSE IS ON FIRE", "oh man"];
+
+class TodoList extends React.Component {
+	render() {
+		return (
+				<div>
+					<ul>
+						{this.props.dummyData.map((task) => <li>{task}</li>)}
+					</ul>
+				</div>
+			)
+	}
+}
+
+// class Todo extends React.Component {
+// 	render() {}
+// }
 
 
 
-
-
-
-ReactDOM.render(<p>ricky HOT everyday</p>,
+ReactDOM.render(<TodoList dummyData={dummyData} />,
 	document.getElementById('root'));
